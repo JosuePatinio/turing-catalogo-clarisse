012567d8b048b9d968a929c5b15a74518ee16912
Josué Emmanuel Patiño Magaña <josuepatinomagana@gmail.com>
2026-03-07 13:40:17 -0600

    Preparacion para mensaje de error en autenticacion

--- frontend/includes/auth_check.php
+++ frontend/includes/auth_check.php
@@ -1,17 +1,25 @@
 <?php
 session_start();
 
-// Función para proteger páginas de administración
-function admin_only() {
-    if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'admin') {
-        // Si no es admin, lo mandamos al login de inmediato
-        header("Location: login.php");
+function requerir_login(){
+    if(!isset($_SESSION['id_usuario'])){
+        header("Location: login.php?error=auth_required");
         exit();
     }
 }
 
-// Función para saber si alguien está logueado (para no imprimir header)
-function is_logged_in() {
+function admin_only() {
+    requerir_login();
+    if ($_SESSION['rol'] !== 'admin') {
+        // Si no es admin, lo mandamos al login de inmediato
+        header("Location: index.php?error=admin_only");
+        exit();
+    }
+}
+
+
+// Funcion para saber si alguien esta logueado
+function esta_logeado() {
     return isset($_SESSION['id_usuario']);
 }
 ?>
