173260ca05c482a8cd49c1ef8933e9836e6ac602
Josué Emmanuel Patiño Magaña <josuepatinomagana@gmail.com>
2026-03-07 18:11:46 -0600

    Se añade el stock y el id de categoria para obtenerlos en la edicion.

--- backend/api/get_productos.php
+++ backend/api/get_productos.php
@@ -5,24 +5,23 @@ header("Content-Type: application/json; charset=UTF-8");
 
 //Conexion a la base de datos
 require_once '../config/db.php';
+$id_categoria = isset($_GET['id_categoria']) ? (int)$_GET['id_categoria'] : null;
 
-$sql = "SELECT p.id, p.nombre, p.descripcion, p.precio, p.imagen_url, c.rango_edad
+$sql = "SELECT p.id, p.nombre, p.descripcion, p.precio, p.stock, p.imagen_url, p.id_categoria, c.rango_edad
         FROM productos p
         LEFT JOIN categorias c ON p.id_categoria = c.id";
 
-$resultado = $conexion->query($sql);
-
-$productos = array();
-
-if ($resultado->num_rows>0){
-    while($fila = $resultado->fetch_assoc()){
-        array_push($productos, $fila);
-    }
-    //Convertir arreglo a JSON
-    echo json_encode($productos);
-}else{
-    echo json_encode([]);
+if ($id_categoria) {
+    $sql .= " WHERE p.id_categoria = $id_categoria";
 }
+$resultado = $conexion->query($sql);
+$productos = [];
 
+
+while($fila = $resultado->fetch_assoc()){
+    $productos[]=$fila;
+}
+//Convertir arreglo a JSON
+echo json_encode($productos);
 $conexion->close();
 ?>
